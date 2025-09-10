70b8fce289b6d6e023feba51c96749492f7b1813
Mahdad Ghasemian <mahdad.ghasemian@gmail.com>
2025-09-10 15:58:26 +0330

    django template

--- python/1/devsearch/projects/views.py
+++ python/1/devsearch/projects/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def projects(request):
-    return render(request, 'projects.html')
+    msg = 'Hello, you are on the projects page'
+    return render(request, 'projects/projects.html', {'message': msg})
 
 def project(request, pk):
-    return render(request, 'single-project.html')
+    return render(request, 'projects/single-project.html')
